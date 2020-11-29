82bd77111f5a01138bde1dcfaf2639b4447e3646
Misha Oleinychenko <m.oleinychenko@gmail.com>
2020-11-29 14:58:36 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const searchInput = document.getElementById("search--input");
+const searchResult = document.getElementById("search--result");
+
+const searchAndFilter = async (searchText) => {
+  const response = await fetch("/capitalByCountry.json");
+  const countries = await response.json();
+
+  // get matches to current input
+  let checkMatches = countries.filter((oneCountry) => {
+    const regex = new RegExp(`^${searchText}`, "gi");
+    if (regex === null) {
+      regex = "no information";
+    }
+    return oneCountry.country.match(regex) || oneCountry.city.match(regex);
+  });
+
+  // if input empty clear it out
+  if (searchInput.value === "") {
+    checkMatches = [];
+    searchResult.innerHTML = "";
+  }
+
+  // shows results in html
+  const outputResults = (matches) => {
+    if (checkMatches.length > 0) {
+      const html = checkMatches
+        .map(
+          (match) => `
+      <div>
+      <h3>${match.country} <span class="text-info">(${match.city})</span> </h3>
+      </div>
+      `
+        )
+        .join("");
+      searchResult.innerHTML = html;
+    }
+  };
+  outputResults(checkMatches);
+};
+
+searchInput.addEventListener("input", () => searchAndFilter(searchInput.value));
---
+++ readme.md
@@ -0,0 +1,12 @@
+## Description
+
+This is a simple search box app, where you can look up countries by their
+capitals and vise versa.
+
+## Installation
+
+It doesn't have any dependencies or packages
+
+## Content
+
+Json file with data and both js and html files with injected bootstrap styling.
